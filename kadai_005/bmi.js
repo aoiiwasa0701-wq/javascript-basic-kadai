5f5ebb35d75ac29b06eb38110048ac61ec541b88
aoiiwasa0701-wq <aoiiwasa0701@outlook.jp>
2026-05-08 14:56:34 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,21 @@
-let weight=68;
-let height=1.7;
+let weight;
+
+weight='体重';
+
+let height;
+
+height='身長';
+
+let bmi;
+
+bmi='体格指数';
+
+weight=68;
+
+height=1.7;
+
+
+
 
 console.log(weight/(height*height));
 
